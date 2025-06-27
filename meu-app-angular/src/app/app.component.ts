d064d0d3126793fb347a5d9f8c16079366a7f180
Bruno Carlos <brunocarlos.souza@hotmail.com>
2025-06-26 21:18:53 -0300

    adding a button in the first page to register employees

--- meu-app-angular/src/app/app.component.ts
+++ meu-app-angular/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule} from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, HomeComponent, HttpClientModule],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
